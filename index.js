b2469b4bdd8bd7e1ed92bfb4b025d0bd31b51759
Yuebai <xyb1114@gmail.com>
2016-01-06 11:13:00 +0100

    upload image and display on the browser again

--- index.js
+++ index.js
@@ -4,16 +4,23 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var multer = require("multer");
-var upload = multer();
+var upload = multer({dest:'uploads/'});//set the destiation for the files uploaded. The files are saved in the same folder as index.js
+var filepath = "";
 
 app.get("/",function(req, res){
     res.sendFile(__dirname+"/index.html")
 });
 
 app.post('/upload_action', upload.single("pdf"), function (req, res) {
-    res.send("Thank you");
+    res.sendFile(__dirname+"/upload.html")
     console.log(req.body);//request.body contains all the text fields
     console.log(req.file);//req.file get the current file
+    filepath = req.file.path;
+});
+
+app.get("/image.jpg",function(req,res){//send file to image src
+    console.log("filepath "+filepath);
+    res.sendFile(__dirname+"/"+filepath);
 });
 
 http.listen(3000, function(){
